fe5ea0b44d0b3ec9d2474a3366eb0c1ba91bb4f2
BlaxBerry <chenjiaxu333@gmail.com>
2025-09-18 00:20:45 +0900

    chore: update env variables

--- src/configs.global.ts
+++ src/configs.global.ts
@@ -1,20 +1,19 @@
 const CONFIGS = {
   SITE: {
-    NAME: `${import.meta.env.ASTRO_APP_NAME} ( ${process.env.NODE_ENV} )`,
+    NAME: `${import.meta.env.ASTRO_APP_SERIES_NAME} ${import.meta.env.ASTRO_APP_NAME} ${process.env.NODE_ENV === "development" ? "( DEV )" : ""}`,
     URL: import.meta.env.ASTRO_SITE_URL,
     BASE: import.meta.env.ASTRO_SITE_BASE,
   },
 
   APP: {
-    NAME: `${import.meta.env.ASTRO_APP_SERIES_NAME} - ${import.meta.env.ASTRO_APP_NAME}`,
+    NAME: `${import.meta.env.ASTRO_APP_NAME}`,
+    NAME_WITH_SERIES: `${import.meta.env.ASTRO_APP_SERIES_NAME} ${import.meta.env.ASTRO_APP_NAME}`,
     AUTHOR: import.meta.env.ASTRO_APP_AUTHOR,
   },
 
   PAGE_NAME: {
-    WORKS: "Works Collection",
-    ABOUT: "About Me",
-    CONTACT: "Contact Me",
-    RESUME: "Resume",
+    WORKS: "作品集",
+    RESUME: "履歷書",
   },
 } as const;
 
