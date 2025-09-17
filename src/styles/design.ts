6d7deeb9b95ed8f36c9f4435749e8c5d997a8ab3
BlaxBerry <chenjiaxu333@gmail.com>
2025-09-17 21:35:22 +0900

    style: modify layout

---
+++ src/styles/design.ts
@@ -0,0 +1,8 @@
+export const DESIGN_CLASSNAME = {
+  COLOR_GRADIENT_BACKGROUND:
+    "bg-gradient-to-r from-purple-600 to-pink-600 dark:from-purple-900 dark:to-pink-900 text-white",
+  COLOR_GRADIENT_TEXT:
+    "text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600 dark:from-purple-900 dark:to-pink-900",
+  COLOR_GRADIENT_BACKGROUND_HOVER:
+    "hover:bg-gradient-to-r hover:from-purple-600 hover:to-pink-600 dark:hover:from-purple-400 dark:hover:to-pink-400",
+} as const;
--- src/styles/global.ts
+++
@@ -1,5 +0,0 @@
-export const SITE_LAYOUT = {
-  HEADER: {
-    HEIGHT: 60,
-  },
-} as const;
