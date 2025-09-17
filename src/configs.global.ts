5ed807f9a1017779c7e8d355060b782b5efe9fce
BlaxBerry <chenjiaxu333@gmail.com>
2025-09-18 00:02:07 +0900

    chore: update env variables

--- src/configs.global.ts
+++ src/configs.global.ts
@@ -1,14 +1,20 @@
 const CONFIGS = {
   SITE: {
-    NAME: import.meta.env.ASTRO_SITE_NAME,
+    NAME: `${import.meta.env.ASTRO_APP_NAME} ( ${process.env.NODE_ENV} )`,
     URL: import.meta.env.ASTRO_SITE_URL,
     BASE: import.meta.env.ASTRO_SITE_BASE,
+  },
+
+  APP: {
+    NAME: `${import.meta.env.ASTRO_APP_SERIES_NAME} - ${import.meta.env.ASTRO_APP_NAME}`,
+    AUTHOR: import.meta.env.ASTRO_APP_AUTHOR,
   },
 
   PAGE_NAME: {
     WORKS: "Works Collection",
     ABOUT: "About Me",
     CONTACT: "Contact Me",
+    RESUME: "Resume",
   },
 } as const;
 
